f0adf31731549d6a4692f5e99f6177a2c927e13a
AbhinavKesani <23eg105g29@anurag.edu.in>
2026-05-21 21:31:28 +0530

    fixed bcryptjs import

--- Services/authService.js
+++ Services/authService.js
@@ -1,49 +1,57 @@
 import { UserTypeModel } from "../models/UserTypeModel.js";
 import jwt from "jsonwebtoken";
-import { hash, compare } from "bcryptjs";
+import bcrypt from "bcryptjs";
 
 export const register = async (userObj) => {
-  //create document
+  // create document
   const userDoc = new UserTypeModel(userObj);
-  //validate for empty password
+
+  // validate
   await userDoc.validate();
-  //hash the password and replace
-  userDoc.password = await hash(userDoc.password, 10);
-  //save
+
+  // hash password
+  userDoc.password = await bcrypt.hash(userDoc.password, 10);
+
+  // save user
   const created = await userDoc.save();
-  //convet doc to obj to remove password
+
+  // convert to object
   const newUserObj = created.toObject();
-  //remove password
+
+  // remove password
   delete newUserObj.password;
-  //return user object with password
+
   return newUserObj;
 };
 
-//authenticate function
+// authenticate function
 export const authenticate = async ({ email, password }) => {
-  //check user with email and role
+  // find user
   const user = await UserTypeModel.findOne({ email });
-  if (!user) {
-    const err = new Error("inavalid email ");
-    err.status = 401;
-    throw err;
-  }
-  //if user vaild , but blocked by admin
 
-  //compare password
-  const isMatch = await compare(password, user.password);
-  if (!isMatch) {
-    const err = new Error("inavalid password");
+  if (!user) {
+    const err = new Error("invalid email");
     err.status = 401;
     throw err;
   }
-  //is active or not
+
+  // compare password
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    const err = new Error("invalid password");
+    err.status = 401;
+    throw err;
+  }
+
+  // blocked check
   if (user.isActive === false) {
-    const err = new Error("your account is blocked . please contcat admin");
+    const err = new Error("your account is blocked. please contact admin");
     err.status = 403;
     throw err;
   }
-  //generate token
+
+  // generate token
   const token = jwt.sign(
     {
       userId: user._id,
@@ -54,9 +62,12 @@ export const authenticate = async ({ email, password }) => {
     process.env.JWT_SECRET,
     {
       expiresIn: "1h",
-    },
+    }
   );
+
   const userObj = user.toObject();
+
   delete userObj.password;
+
   return { token, user: userObj };
 };
